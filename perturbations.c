10f3e5f458c306defd03ffda33fdd0df60714c83
FarbodHassani <farbod.hassani@gmail.com>
2017-11-03 13:49:39 +0100

    Approximations checks are added, h''=constant in time!

--- perturbations.c
+++ perturbations.c
@@ -5206,19 +5206,42 @@ int perturb_einstein(
   //Some notes:
   //.//source/background.c:      double Omega = exp(alpha_M0*pow(a, beta)/beta)-1;
   //
+  
+ // Printing the fields
+ // if (fabs(a-0.01)<0.001)
+ // {
+ //   double pi_newton = ppw->pvecmetric[ppw->index_mt_vx_smg]+ppw->pvecmetric[ppw->index_mt_alpha]; // scalar field in Newtonian gauge --> Gauge transformation: pi(Newt)=pi(Sync)+ alpha
+ //   double pi_prime_newton=ppw->pvecmetric[ppw->index_mt_vx_prime_smg]+ppw->pvecmetric[ppw->index_mt_alpha_prime];
+ //   double pi_prime_prime_newton=ppw->pvecmetric[ppw->index_mt_vx_prime_prime_smg] ; // We take alpha_prime_prime=0 in the begining
+ //   // double pinewton = y[ppw->pv->index_pt_vx_smg]+alpha*0.0;
+ //   // double piprimen = y[ppw->pv->index_pt_vx_prime_smg];
+ //   // double pidprimn = ppw->pvecmetric[ppw->index_mt_vx_prime_prime_smg];
+ //    FILE * out=fopen("./output/Kessence_info.dat","a");
+ //    fprintf(out,"%e\t%e\t%e\t%e\t%e\n",k,a,pi_newton,pi_prime_newton,pi_prime_prime_newton,rho_smg);
+ //    fclose(out);
+ //   // fprintf(stdout,"%e\t%e\t%e\n",k,ppw->pv->index_pt_vx_smg,ppw->pvecmetric[ppw->index_mt_vx_smg],);
+ //  }
 
 
+
+
+
+  // Checking the approximations
   //loop to take the scalar field and the derivatives in each k  over a redshift bins (FH)
   if (fabs(a-0.01)<0.001)
   {
     double pi_newton = ppw->pvecmetric[ppw->index_mt_vx_smg]+ppw->pvecmetric[ppw->index_mt_alpha]; // scalar field in Newtonian gauge --> Gauge transformation: pi(Newt)=pi(Sync)+ alpha
     double pi_prime_newton=ppw->pvecmetric[ppw->index_mt_vx_prime_smg]+ppw->pvecmetric[ppw->index_mt_alpha_prime];
     double pi_prime_prime_newton=ppw->pvecmetric[ppw->index_mt_vx_prime_prime_smg] ; // We take alpha_prime_prime=0 in the begining
+    double h_prime_prime=ppw->pvecmetric[ppw->index_mt_h_prime_prime]
     // double pinewton = y[ppw->pv->index_pt_vx_smg]+alpha*0.0;
     // double piprimen = y[ppw->pv->index_pt_vx_prime_smg];
     // double pidprimn = ppw->pvecmetric[ppw->index_mt_vx_prime_prime_smg];
-     FILE * out=fopen("./output/Kessence_info.dat","a");
-     fprintf(out,"%e\t%e\t%e\t%e\t%e\n",k,a,pi_newton,pi_prime_newton,pi_prime_prime_newton);
+     FILE * out=fopen("./output/Kessence_icheck.dat","a");
+     //Test for h, We expect h'' be constant in Matter dominated era!
+     fprintf(out,"%e\t%e\t%e\t%e\t%e\t%e\n",k,a,h_prime_prime);
+    //  fprintf(out,"%e\t%e\t%e\t%e\t%e\t%e\n",k,a, ,ppw->pvecmetric[ppw->index_mt_h_prime_prime],ppw->pvecmetric[ppw->index_mt_alpha],ppw->pvecmetric[ppw->index_mt_alpha_prime],pi_newton,pi_prime_newton,pi_prime_prime_newton,rho_smg,ppw->pvecmetric[ppw->index_mt_alpha],pv->index_pt_eta);
+
      fclose(out);
     // fprintf(stdout,"%e\t%e\t%e\n",k,ppw->pv->index_pt_vx_smg,ppw->pvecmetric[ppw->index_mt_vx_smg],);
    }
